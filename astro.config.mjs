61acca9a0c7b01188a30cd566135ed33086a2484
Théo Senoussaoui <theosen95@gmail.com>
2023-08-05 12:10:08 +0200

    chore: upgrading packages and conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import compress from 'astro-compress';
 import vanillaExtract from 'astro-vanilla-extract';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vanillaExtract(), compress(), sitemap(), preact()],
   output: 'server',
   site: 'https://azertykeycaps.fr',
-  adapter: vercel()
+  adapter: vercel(),
+  experimental: {
+    viewTransitions: true
+  }
 });
