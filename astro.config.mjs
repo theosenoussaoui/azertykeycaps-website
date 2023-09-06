594db6dc0b15a0cd3cb74b2ddf2d03e3bdaa37e9
Théo Senoussaoui <theosen95@gmail.com>
2023-09-06 22:15:28 +0200

    feat: adding first astro pwa poc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import vanillaExtract from 'astro-vanilla-extract';
 import sitemap from '@astrojs/sitemap';
+import AstroPWA from '@vite-pwa/astro';
 
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vanillaExtract(), sitemap(), preact()],
+  integrations: [vanillaExtract(), sitemap(), preact(), AstroPWA()],
   output: 'server',
   site: 'https://azertykeycaps.fr',
   adapter: vercel({})
