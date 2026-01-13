3e9b287b9f57e66b5f14eb4022a916aa66b65eca
Théo Senoussaoui <theosen95@gmail.com>
2026-01-13 01:51:14 +0100

    chore: migration in dev & prod

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260112_211705 from "./20260112_211705";
+import * as migration_20260113_004942 from "./20260113_004942";
 
 export const migrations = [
   {
     up: migration_20260112_211705.up,
     down: migration_20260112_211705.down,
     name: "20260112_211705",
+  },
+  {
+    up: migration_20260113_004942.up,
+    down: migration_20260113_004942.down,
+    name: "20260113_004942",
   },
 ];
